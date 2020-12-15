a1a5d2080025debfe5645895385afcf2b88349c6
rahmansaleh7 <rahman.saleh7@gmail.com>
2020-12-15 14:46:51 +0700

    First

---
+++ inputan_cmd.py
@@ -0,0 +1,7 @@
+data = raw_input("\nMasukan Pilihan Anda [1,2,3]: ")
+if data == "1":
+    print('\nPilihan Anda Salah\n')
+if data == "2":
+    print('\nPilihan Anda Salah\n')
+if data == "3":
+    print('\nPilihan Anda Benar\n\n')
