54139a797d586db610d4f6422816d4fca8b88594
rahmansaleh7 <rahman.saleh7@gmail.com>
2021-01-11 16:06:23 +0700

    next commit

---
+++ loop.py
@@ -0,0 +1,9 @@
+
+
+x = 10
+
+while x < 100:
+    print(x)
+    x += 1
+
+print("done\n")
---
+++ type_data.py
@@ -0,0 +1,20 @@
+#String
+ab = ""
+
+
+# Number
+bc = 10
+cd = 12.2
+age = 12l #L fungsi l menjadikan integer menjadi long integer
+x = 3.14j #j fungsi j adalah Complex number (angka real dan angka imajiner)
+
+#List = Array
+student = ["Arman","Firman",10.2,2]
+#Tupple = readonly Array
+student = ("Arman","Firman")
+
+#Dictionary = array assosiated, map
+student = {"key":"value","name":"Arman","age":"17"}
+
+print student["name"]
+print student["age"]
